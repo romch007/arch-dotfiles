e7050da4f39783db45fbf9a3f125f55fc82af05f
Romain Chardiny <romain.chardiny@gmail.com>
2022-11-27 22:01:28 +0100

    add battery and sound stuff

--- dot_dwm/executable_autostart.sh
+++ dot_dwm/executable_autostart.sh
@@ -6,7 +6,7 @@ cpu() {
 }
 
 disk() {
-	result=$(df -h | grep sda1 | awk 'OFS="/" {print $3,$2}')
+	result=$(df -h | grep sda2 | awk 'OFS="/" {print $3,$2}')
 	echo -e " $result"
 }
 
@@ -15,12 +15,36 @@ mem() {
 	echo -e " $result"
 }
 
+battery() {
+  battery_status=$(cat /sys/class/power_supply/BAT0/status)
+  if [ "$battery_status" = "Charging" ]
+  then
+    icon=""
+  else
+    icon=""
+  fi
+
+  result=$(cat /sys/class/power_supply/BAT0/capacity)
+  echo -e "$icon $result%"
+}
+
 custom_date() {
   result=$(date +"%A %B %d %H:%M")
   echo -e $result
 }
 
+volume() {
+  volume_level=$(pamixer --get-volume)
+  muted=$(pamixer --get-mute)
+  if [ "$muted" = "true" ]
+  then
+    echo -e " [Muted]"
+  else
+    echo -e " $volume_level%"
+  fi
+}
+
 while true; do
-  xsetroot -name "$(cpu) | $(mem) | $(disk) | $(custom_date)"
+  xsetroot -name "$(volume) | $(battery) | $(cpu) | $(mem) | $(disk) | $(custom_date)"
 	sleep 2s
 done
