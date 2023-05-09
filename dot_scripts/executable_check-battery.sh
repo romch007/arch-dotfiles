93d7dfd76a172e57b3af304f1ca96a24c92a3009
Romain Chardiny <romain.chardiny@gmail.com>
2023-05-09 15:23:52 +0200

    improve check battery script

--- dot_scripts/executable_check-battery.sh
+++ dot_scripts/executable_check-battery.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+state=$(cat /sys/class/power_supply/BAT0/status)
 percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 limit=20
 
-if [ "$percentage" -le "$limit" ]; then
+if [ "$percentage" -le "$limit" ] && [ "$state" = "Discharging" ]; then
   notify-send -u critical "Battery is at $percentage%!"
 fi
